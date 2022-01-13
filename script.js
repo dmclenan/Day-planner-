e6ee1262386ef98f6923729e9648ab8c896535b8
Dondre Mclenan <d.mclenan@gmail.com>
2022-01-13 14:38:08 -0500

    udated day planner

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
+// My ready function
 $(document).ready(function () {
     // MY daily date on the page
     $("#currentDay").text(moment().format("MMM Do YY"));
     // I want the colors to change with the time
     var currentTime = parseInt(moment().format('HH'));
     // Compare current time to the time on the time block for each row
-    //const rowTime = document.getElementById("9");
-    // const rowTime = document.getElementById("10");
     $(".saveBtn").on("click", function () {
-        //var userInput = document.getElementById("description9").value
-        //localStorage.setItem('savebtn', userInput);
- const value = $(this).siblings(".description").val()
-    const rowTime = $(this).parent().attr("id")
-    localStorage.setItem(value, rowTime)
+        // Storing values to varibles
+        const value = $(this).siblings(".description").val()
+        const rowTime = $(this).parent().attr("id")
+        localStorage.setItem(rowTime, value)
     });
-   
     //Loop through each row get hour and determine the past,present,future//
     function updatedHour() {
         var currentHour = moment().hours()
@@ -31,22 +28,16 @@ $(document).ready(function () {
             }
         });
     }
+    // Calling the updatedHour function to run
     updatedHour();
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-
     // Saving descriptions to local storage 
-    //var userInput9 = localStorage.getItem('savebtn9') || ''
-    //document.getElementById("description9").value = userInput9
-    // Creating a EventListener to save when clicked
-
-    // Saving descriptions to local storage 
-    //var userInput10 = localStorage.getItem('savebtn') || ''
-    //document.getElementById("description10").value = userInput10
-    // Creating a EventListener to save when clicked
-    //document.getElementById("savebtn").addEventListener("click", function () {
-        //var userInput = document.getElementById("description10").value
-        //localStorage.setItem('savebtn', userInput);
-
-    //});
-
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 })
